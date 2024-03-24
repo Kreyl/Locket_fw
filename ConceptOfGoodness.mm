d4789c1156264626da1d3b5aef8dad547b354095
kreyl <laystoll@yandex.ru>
2024-03-24 12:09:12 +0300

    + Mindmap

---
+++ ConceptOfGoodness.mm
@@ -0,0 +1,95 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="ConceptOfGoodness" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1711269741787"><hook NAME="MapStyle">
+    <properties fit_to_viewport="false;" associatedTemplateLocation="template:/KL_standard-1.6.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_896312434" MAX_WIDTH="600 px" COLOR="#000000" STYLE="bubble">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_896312434" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode TEXT="OK">
+<icon BUILTIN="button_ok"/>
+</stylenode>
+<stylenode TEXT="NotOk">
+<icon BUILTIN="button_cancel"/>
+</stylenode>
+<stylenode TEXT="Question">
+<icon BUILTIN="help"/>
+</stylenode>
+<stylenode TEXT="Attention">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="Stopper">
+<icon BUILTIN="stop-sign"/>
+</stylenode>
+<stylenode TEXT="Waiting">
+<icon BUILTIN="hourglass"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" BACKGROUND_COLOR="#ccffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" BACKGROUND_COLOR="#ffffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" BACKGROUND_COLOR="#ffcccc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Виды устройств" POSITION="bottom_or_right" ID="ID_718500694" CREATED="1711269742985" MODIFIED="1711269749278">
+<node TEXT="СимволВеры" ID="ID_1420335822" CREATED="1711269750014" MODIFIED="1711269759309">
+<node TEXT="Благость" ID="ID_181379895" CREATED="1711269847492" MODIFIED="1711269852674">
+<node TEXT="Индикация" ID="ID_316985252" CREATED="1711269772467" MODIFIED="1711269859807">
+<node TEXT="[0;6199]" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_52455970" CREATED="1711269790360" MODIFIED="1711269835044"/>
+<node TEXT="[7200; 14399]" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1608295729" CREATED="1711269807655" MODIFIED="1711269835882"/>
+<node TEXT="[14400; 21600]" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_795245128" CREATED="1711269820158" MODIFIED="1711269836573"/>
+</node>
+<node TEXT="Уменьшается на 1 раз в секунду" ID="ID_821752006" CREATED="1711269862712" MODIFIED="1711269874311"/>
+</node>
+</node>
+<node TEXT="Мастерский" ID="ID_829722576" CREATED="1711269905559" MODIFIED="1711269909871">
+<node TEXT="Кнопка вкл-выкл источник благости" ID="ID_555877681" CREATED="1711269910636" MODIFIED="1711269932932">
+<node TEXT="Благость растёт +Х раз в секунду" ID="ID_1044206491" CREATED="1711269933697" MODIFIED="1711269949637"/>
+</node>
+</node>
+<node TEXT="Отвёртка" ID="ID_719253113" CREATED="1711269956980" MODIFIED="1711269960426">
+<node TEXT="+Х благости" ID="ID_1229539886" CREATED="1711269970217" MODIFIED="1711269983757"/>
+<node TEXT="-Х благости" ID="ID_551983833" CREATED="1711270097640" MODIFIED="1711270103218"/>
+</node>
+<node TEXT="Люстра (место силы)" ID="ID_1035288015" CREATED="1711270015805" MODIFIED="1711270327800">
+<node TEXT="Снижает благость на Х в секунду" ID="ID_1757517833" CREATED="1711270023039" MODIFIED="1711270232505">
+<node TEXT="Или на Y в секунду" POSITION="bottom_or_right" ID="ID_1228784686" CREATED="1711270082220" MODIFIED="1711270090130">
+<node TEXT="Осквернено хаосом" ID="ID_1305090306" CREATED="1711270109252" MODIFIED="1711270117422"/>
+</node>
+</node>
+<node TEXT="Или увеличивает на Х в секунду" ID="ID_1398000199" CREATED="1711270176279" MODIFIED="1711270245782"/>
+<node TEXT="Можно настраивать X, Y" ID="ID_633305743" CREATED="1711270255136" MODIFIED="1711270264525"/>
+</node>
+<node TEXT="Пилюли" ID="ID_103596658" CREATED="1711270271589" MODIFIED="1711270279277">
+<node TEXT="Увеличивают благость" ID="ID_840342309" CREATED="1711270292176" MODIFIED="1711270298345"/>
+<node TEXT="Уменьшают благость" ID="ID_1664972404" CREATED="1711270298557" MODIFIED="1711270303721"/>
+<node TEXT="Настраивают места силы" ID="ID_372317436" CREATED="1711270307058" MODIFIED="1711270317770"/>
+</node>
+</node>
+</node>
+</map>
