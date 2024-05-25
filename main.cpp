5e02e6019d52a305a678990492efcb0da6ae3517
kreyl <laystoll@yandex.ru>
2024-05-25 13:51:19 +0300

    3 btns RC

--- main.cpp
+++ main.cpp
@@ -18,9 +18,9 @@ Vibro_t Vibro {VIBRO_SETUP};
 union rPkt_t {
     uint32_t DW32[2];
     struct {
-        int32_t Indx;
-        uint32_t TheWord = 0xCa110fEa;
-    } __attribute__((__packed__));
+        uint32_t salt;
+        uint32_t H;
+    };
     rPkt_t& operator = (const rPkt_t &Right) {
         DW32[0] = Right.DW32[0];
         DW32[1] = Right.DW32[1];
@@ -28,7 +28,7 @@ union rPkt_t {
     }
 } __attribute__ ((__packed__));
 
-rPkt_t PktTx;
+rPkt_t pkt_tx;
 #define RPKT_LEN    sizeof(rPkt_t)
 
 cc1101_t CC(CC_Setup0);
@@ -44,7 +44,9 @@ int main(void) {
     // Check if no btn pressed
     PinSetupInput(BTN1_PIN, pudPullDown);
     PinSetupInput(BTN2_PIN, pudPullDown);
-    if(Sleep::WasInStandby() and PinIsLo(BTN1_PIN) and PinIsLo(BTN2_PIN)) SleepNow(450); // no btn, sleep no long
+    PinSetupInput(BTN3_PIN, pudPullDown);
+    // Check if no btn: sleep no long
+    if(Sleep::WasInStandby() and PinIsLo(BTN1_PIN) and PinIsLo(BTN2_PIN) and PinIsLo(BTN3_PIN)) SleepNow(450);
 
     // ==== Init Vcore & clock system ====
     SetupVCore(vcore1V2);
@@ -59,32 +61,46 @@ int main(void) {
     // ==== Init hardware ====
     Uart.Init();
     Vibro.Init();
+    Led.Init();
 //    if(!Sleep::WasInStandby()) {
         Printf("\r%S %S\r", APP_NAME, XSTRINGIFY(BUILD_TIME));
         Clk.PrintFreqs();
 //    }
 
     if(CC.Init() == retvOk) {
+        pkt_tx.salt = 0xCa110fEa;
         if(Sleep::WasInStandby()) {
             // Vibrate accordingly
-            if(PinIsHi(BTN1_PIN)) Vibro.StartOrRestart(vsqBrr);
-            else if(PinIsHi(BTN2_PIN)) Vibro.StartOrRestart(vsqBrrBrr);
+            if(PinIsHi(BTN1_PIN)) {
+                Vibro.StartOrRestart(vsqBrr);
+                pkt_tx.H = 0;
+                Led.SetColor(clRed);
+            }
+            else if(PinIsHi(BTN2_PIN)) {
+                Vibro.StartOrRestart(vsqBrrBrr);
+                pkt_tx.H = 120;
+                Led.SetColor(clGreen);
+            }
+            else if(PinIsHi(BTN3_PIN)) {
+                Vibro.StartOrRestart(vsqBrrBrrBrr);
+                pkt_tx.H = 240;
+                Led.SetColor(clBlue);
+            }
             // CC set params
             CC.SetPktSize(RPKT_LEN);
             CC.SetChannel(0); // Same as RX
             CC.SetTxPower(CC_PwrPlus5dBm);
             CC.SetBitrate(CCBitrate100k);
             // Transmit what needed
-            while(PinIsHi(BTN1_PIN) or PinIsHi(BTN2_PIN)) {
-                PktTx.Indx = PinIsHi(BTN1_PIN)? 0 : 1;
+            while(PinIsHi(BTN1_PIN) or PinIsHi(BTN2_PIN) or PinIsHi(BTN3_PIN)) {
                 CC.Recalibrate();
-                CC.Transmit(&PktTx, RPKT_LEN);
+                CC.Transmit(&pkt_tx, RPKT_LEN);
+                chThdSleepMilliseconds(7);
             }
             CC.EnterPwrDown();
             SleepNow(270); // To repeat transmission soon
         }
         else { // indicate powering on
-            Led.Init();
             Led.StartOrRestart(lsqStart);
             Vibro.StartOrRestart(vsqBrrBrr);
             chThdSleepMilliseconds(999);
@@ -93,7 +109,6 @@ int main(void) {
         }
     }
     else { // CC failure
-        Led.Init();
         Led.StartOrRestart(lsqFailure);
         chThdSleepMilliseconds(207);
         SleepNow(2700);
