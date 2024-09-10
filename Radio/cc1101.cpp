83dd3b1b7bc183683a78f1c9767ecd49b1cd021f
kreyl <laystoll@yandex.ru>
2024-09-10 21:29:07 +0300

    RC

--- Radio/cc1101.cpp
+++ Radio/cc1101.cpp
@@ -75,7 +75,7 @@ retv cc1101_t::Init() {
     WriteRegister(CC_MCSM1, CC_MCSM1_VALUE);
 
     IGdo0.EnableIrq(IRQ_PRIO_HIGH);
-    Printf("CC init ok\r");
+//    Printf("CC init ok\r");
     return retv::Ok;
 }
 
